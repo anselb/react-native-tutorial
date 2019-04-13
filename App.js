ce83703b24ec4150dada4027ee6de4ada714597f
Ansel Bridgewater <iamansel@gmail.com>
2019-04-13 14:30:42 -0700

    Added weather api call and env file

--- App.js
+++ App.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WEATHER_KEY } from 'react-native-dotenv'
 
 import DisplayWeather from './Components/DisplayWeather'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      location: null,
+      weather: null
+    }
+  }
+
+  loadWeather() {
+    console.log(WEATHER_KEY)
+    const apikey = WEATHER_KEY
+    const { latitude, longitude } = this.state.location.coords
+    const units = 'Imperial'
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=${units}`
+
+    fetch(url)
+      .then(res => res.json())
+      .then(json => this.setState({ weather: json }))
+      .catch(err => console.log(err.message))
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      console.log(location)
+      this.setState({ location })
+      this.loadWeather()
+    }, (err) => {
+      console.log(err.message)
+    })
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
